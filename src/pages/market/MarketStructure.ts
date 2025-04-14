249e540a0f9f2c078c95bf4d11c71439eb512870
Cemil Şahin <cemilsahin.dev@gmail.com>
2025-04-14 23:01:51 +0300

    fix body vertical scaling & add new currencies

--- src/pages/market/MarketStructure.ts
+++ src/pages/market/MarketStructure.ts
@@ -31,18 +31,21 @@ export interface AssetInfo {
     total: number;
 }
 
-export const filterCodesApiluna = ["USDTRY", "EURTRY", "CHFTRY", "EURUSD", "ALTIN",  "ONS", 
-                                   "KULCEALTIN", "CEYREK_YENI", "YARIM_YENI", "TEK_YENI", "ATA_YENI",   
+export const filterCodesApiluna = ["USDTRY", "EURTRY", "ALTIN",  "KULCEALTIN", "GBPTRY", 
+                                   "CHFTRY", "AUDTRY", "CADTRY", "SARTRY", "JPYTRY", "AEDTRY",
+                                   "CEYREK_YENI", "YARIM_YENI", "TEK_YENI", "ATA_YENI",   
                                    "ATA5_YENI", "GREMESE_YENI",  "AYAR22", "AYAR14", "GUMUSTRY"];
-export const adjustedCodesApiluna = ["USD", "EUR", "CHF", "EUR / USD", "ALTIN",  "ONS", 
-                                     "KÜLÇE ALTIN", "ÇEYREK YENİ", "YARIM YENİ", "TAM YENİ", "ATA YENİ",   
-                                     "ATA YENİ 5'Lİ", "GREMESE YENİ",  "22 AYAR", "14 AYAR", "GÜMÜŞ"];
+export const adjustedCodesApiluna = ["Dolar", "Euro", "Gram Altın", "Külçe Altın", "Pound", "Isveç Frankı", 
+                                     "Avustralya Doları", "Kanada Doları", "(SAR) Riyal", "Japon Yeni",
+                                     "(AED) Dirham", "Çeyrek Yeni", "Yarım Yeni", "Tam Yeni", "Ata Yeni",   
+                                     "Ata Yeni 5'li", "Gremese Yeni",  "22 Ayar", "14 Ayar", "Gümüş"];
 
 export const codeMap: Record<string, string> = 
-                        { TRYTRY: "Türk Lirasi", USDTRY: "Dolar", EURTRY: "Euro", CHFTRY: "Isveç Franki", ALTIN: "Gram Altin", KULCEALTIN: "Külçe Altin", 
-                          CEYREK_YENI: "Çeyrek Yeni", YARIM_YENI: "Yarim Yeni", TEK_YENI: "Tam Yeni", ATA_YENI: "Ata Yeni", 
-                          ATA5_YENI: "Ata Yeni 5'li", GREMESE_YENI: "Gremese Yeni", AYAR22: "22 Ayar", AYAR14: "14 Ayar", 
-                          GUMUSTRY: "Gümüş" };
+                        { TRYTRY: "Türk Lirasi", USDTRY: "Dolar", EURTRY: "Euro", ALTIN: "Gram Altin", KULCEALTIN: "Külçe Altin", 
+                          GBPTRY: "Pound", CHFTRY: "Isveç Franki", AUDTRY: "Avustralya Dolari", CADTRY: "Kanada Dolari", SARTRY: "(SAR) Riyal",
+                          JPYTRY: "Japon Yeni", AEDTRY: "(AED) Dirham", CEYREK_YENI: "Çeyrek Yeni", YARIM_YENI: "Yarim Yeni", 
+                          TEK_YENI: "Tam Yeni", ATA_YENI: "Ata Yeni", ATA5_YENI: "Ata Yeni 5'li", GREMESE_YENI: "Gremese Yeni", 
+                          AYAR22: "22 Ayar", AYAR14: "14 Ayar", GUMUSTRY: "Gümüş", PLATIN: "Platin", PALADYUM: "Paladyum"};
 
 export const filterCodesTruncgil = ["USD", "EUR", "CHF", "GBP", "GRA",  "ONS", 
                                     "HAS", "CEY", "YAR", "TAM", "CUM", "BES",  
@@ -51,9 +54,9 @@ export const adjustedCodesTruncgil = ["USD / TRY", "EUR / TRY", "CHF / TRY", "GB
                                       "KÜLÇE ALTIN", "ÇEYREK ALTIN", "YARIM ALTIN", "TAM ALTIN", "ATA ALTIN",   
                                       "ATA YENİ 5'Lİ", "REŞAT ALTIN", "GREMESE",  "22 AYAR", "14 AYAR", "GÜMÜŞ"];
 
-export const currencyCodes = ["TRY", "USD", "EUR", "CHF"];
-export const currencyCodeMap = {TRY: "TRY", USD: "USD", EUR: "EUR", CHF: "CHF"};
-export const currencyFilterCodesApiluna = ["TRYTRY", "USDTRY", "EURTRY", "CHFTRY"];
+export const currencyCodes = ["TRY", "USD", "EUR", "GBP"];
+export const currencyCodeMap = {TRY: "TRY", USD: "USD", EUR: "EUR", GBP: "GBP"};
+export const currencyFilterCodesApiluna = ["TRYTRY", "USDTRY", "EURTRY", "GBPTRY"];
 const currencyValue = [1, 0, 0, 0];
 
 export interface CurrencyItem {
